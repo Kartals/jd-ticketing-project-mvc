a5d0df3b9a9902647024f52db911b2fcd8d37052
Kartals <bkrprs@icloud.com>
2020-11-27 00:45:25 +0100

    ticketing project MVC

--- src/main/java/com/cybertek/controller/UserController.java
+++ src/main/java/com/cybertek/controller/UserController.java
@@ -2,6 +2,9 @@ package com.cybertek.controller;
 
 
 import com.cybertek.dto.UserDTO;
+import com.cybertek.service.RoleService;
+import com.cybertek.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +14,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired    //field injection
+    RoleService roleService;
+
+    @Autowired
+    UserService userService;
+
     // @GetMapping({"/create","/add","/initialize"}) --> we can define more than one endpoint in @GetMapping. bu şu anlama gelmekte, tüm bu endpointler aynı html viewi vermektedir
     @GetMapping("/create")
     public String createUser(Model model){
 
         model.addAttribute("user", new UserDTO());
+        model.addAttribute("roles",roleService.findAll());
+        model.addAttribute("users",userService.findAll());
+
         return "user/create";
     }
 }
