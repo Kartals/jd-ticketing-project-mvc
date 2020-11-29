4db84730c54b2ef97fdfe1de32281f0bfdd564c0
Kartals <bkrprs@icloud.com>
2020-11-29 04:04:46 +0100

    ticketing project MVC

--- src/main/java/com/cybertek/SpringMvcProjectManagementApplication.java
+++ src/main/java/com/cybertek/SpringMvcProjectManagementApplication.java
@@ -3,6 +3,7 @@ package com.cybertek;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 
 @SpringBootApplication
--- src/main/java/com/cybertek/controller/UserController.java
+++ src/main/java/com/cybertek/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -29,10 +30,10 @@ public class UserController {
         model.addAttribute("roles",roleService.findAll());
         model.addAttribute("users",userService.findAll());
 
-        return "user/create";
+        return "/user/create";
     }
 
-    @PostMapping("create")
+    @PostMapping("/create")
     public String insertUser(UserDTO user,Model model){
 
         userService.save(user);
@@ -41,6 +42,30 @@ public class UserController {
         model.addAttribute("roles",roleService.findAll());
         model.addAttribute("users",userService.findAll());
 
-        return "user/create";
+        return "/user/create";
+    }
+
+    @GetMapping("/update{username}")
+    public String editUser(@PathVariable("username") String username, Model model){
+
+        model.addAttribute("user",userService.findById(username));
+        //burada userService.findById(username) kullanmamızın sebebi: update yaptığımızda karşımıza çıkacak view'da update etmek isteyeceğimiz usernamein bilgilerini görmemiz gerektiği,
+        // yani sıfır bir object (new UserDTO() gibi) oluşturmuyoruz, var olan objectin bilgilerine ulaşıyoruz
+        model.addAttribute("users",userService.findAll());
+        model.addAttribute("roles",roleService.findAll());
+
+        return "/user/update";
+    }
+
+    @PostMapping("/update{username}")
+    public String updateUser(@PathVariable("username") String username,UserDTO user, Model model){
+
+        userService.update(user);
+        model.addAttribute("user",new UserDTO());
+        model.addAttribute("roles",roleService.findAll());
+        model.addAttribute("users",userService.findAll());
+
+
+        return "/user/create";
     }
 }
