c440f8af22f6caef2707750db302b5366a891ec2
Kartals <bkrprs@icloud.com>
2020-12-04 08:36:39 +0100

    ticketing project MVC

--- src/main/java/com/cybertek/bootstrap/DataGenerator.java
+++ src/main/java/com/cybertek/bootstrap/DataGenerator.java
@@ -82,7 +82,7 @@ public class DataGenerator implements CommandLineRunner {
 
 
         TaskDTO task1 = new TaskDTO(1l,project1,user8,"Controller","Request Mapping",Status.IN_PROGRESS,LocalDate.now().minusDays(4));
-        TaskDTO task2 = new TaskDTO(2L,project3,user3,"Configuration","Database Connnection",Status.COMPLETE,LocalDate.now().minusDays(12));
+        TaskDTO task2 = new TaskDTO(2L,project3,user3,"Configuration","Database Connection",Status.COMPLETE,LocalDate.now().minusDays(12));
         TaskDTO task3 = new TaskDTO(3L,project3,user6,"Mapping","One-To-Many",Status.IN_PROGRESS,LocalDate.now().minusDays(8));
         TaskDTO task4 = new TaskDTO(4L,project2,user7,"Dependency Injection","Autowired",Status.UAT_TEST,LocalDate.now().minusDays(20));
         taskService.save(task1);
