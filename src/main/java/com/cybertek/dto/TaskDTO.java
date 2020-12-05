ed709492bcec8c3f14de051674e4db6977d56f1d
Kartals <bkrprs@icloud.com>
2020-12-05 12:27:21 +0100

    ticketing project MVC

--- src/main/java/com/cybertek/dto/TaskDTO.java
+++ src/main/java/com/cybertek/dto/TaskDTO.java
@@ -8,13 +8,12 @@ import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
-
 public class TaskDTO {
 
     private Long id; // bunu yazmamızın sebebi bir id yi primary key olarak kullanmak
@@ -24,4 +23,16 @@ public class TaskDTO {
     private String taskDetail;
     private Status taskStatus;
     private LocalDate assignedDate;
+
+    //manually creating constructor. @AllArgsConstructor ı kaldırdık, id için custom constructors oluşturmak için.
+    public TaskDTO(ProjectDTO project, UserDTO assignedEmployee, String taskSubject, String taskDetail, Status taskStatus, LocalDate assignedDate) {
+        this.project = project;
+        this.assignedEmployee = assignedEmployee;
+        this.taskSubject = taskSubject;
+        this.taskDetail = taskDetail;
+        this.taskStatus = taskStatus;
+        this.assignedDate = assignedDate;
+        this.id= UUID.randomUUID().getMostSignificantBits(); //burada yukarda id'imiz Long olduğundan lon return yapan UUID methodu seçtik
+        //UUID, bir java functiondur ve bize uniq identifier sağlar
+    }
 }
