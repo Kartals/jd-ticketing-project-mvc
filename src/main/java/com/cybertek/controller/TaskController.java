bf71d284bfa4b10526b0eb8e1c7fed3b44de875d
Kartals <bkrprs@icloud.com>
2020-12-05 11:48:55 +0100

    ticketing project MVC

--- src/main/java/com/cybertek/controller/TaskController.java
+++ src/main/java/com/cybertek/controller/TaskController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -35,6 +36,12 @@ public class TaskController {
         return "task/create";
     }
 
+    @PostMapping("/create")
+    public String insertTask(Model model, TaskDTO task){
+
+        taskService.save(task);
+        return "redirect:task/create";
+    }
 
 
 }
